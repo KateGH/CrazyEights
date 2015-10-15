fbbd690fd6b080e09a39bfc1a987674b208e4a4b
mizlin <you@example.com>
2015-10-15 20:47:16 +0200

    changeTurn & p1/p2 objects

--- CrazyEights.java
+++ CrazyEights.java
@@ -1,19 +1,41 @@
 import java.util.Scanner;
 
-public class CrazyEights {
+public class CrazyEights { // *1
 
 	public boolean endGame = false;
 	public Scanner keyboard = new Scanner(System.in);
 
+	public Player currentPlayer;
+	public Player p1;
+	public Player p2;
+
 	public static void main(String[] args) {
-		new CrazyEights();
+		new CrazyEights(); // create a new instance of *1, will call *1 constructor
 	}
 
+	public CrazyEights() { // constructor of *1
 
-	public CrazyEights() {		
+		System.out.print("Enter your name: ");
+
+		this.p1 = new Player(keyboard.next(), 0);
+		this.p2 = new Player("Computer", 0);
+
 		while (!endGame) {
 			// initGame();
+			currentPlayer = p1;
+
+			// game play
+			changeTurn();
 			checkRestart();
+		}
+	}
+
+	public void changeTurn() {
+		if (currentPlayer == p1) {
+			currentPlayer = p2;
+
+		} else {
+			currentPlayer = p1;
 		}
 	}
 
@@ -32,7 +54,7 @@ public class CrazyEights {
 				endGame = false;
 
 			} else if (userInput == 'n') {
-				System.out.println("Bye bye!");
+				System.out.println("Bye bye " + p1.name + "!");
 				endGame = true;
 				System.exit(0);
 
